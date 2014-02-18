fe91d68d83d291fcbd94af39dee117acc72618f9
Rory Budnick <rorybudnick@gmail.com>
2014-02-17 20:30:02 -0500

    getting closer to searching api

--- app/controllers/hospitals_controller.rb
+++ app/controllers/hospitals_controller.rb
@@ -1,15 +1,24 @@
 class HospitalsController < ApplicationController
 
-  def search
-    HTTParty.get("http://data.medicare.gov/resource/v287-28n3.json?state=#{params[:state]}&drg_definition=#{params[:drg_definition]}")
+  def index
+    render (:index)
   end
 
-  def index
-    search
+  def search
+    HTTParty.get("http://data.cms.gov/resource/97k6-zzx3.json?provider_state=#{params[:provider_state]}&drg_definition=#{params[:drg_definition].gsub(" ","+")}")
   end
 
   def show
-    listings
+    render(:search)
   end
+
+  #def listing(provider_id)
+    #provider_id = "0" + provider_id.to_s
+    #hospital_info = HTTParty.get("http://data.medicare.gov/resource/v287-28n3.#json?provider_number=#{provider_id}")
+    #puts hospital_info[0]["hospital_name"]
+    #puts hospital_info[0]["address_1"]
+    #puts "#{hospital_info[0]["city"]}, #{hospital_info[0]["state"]}"
+    #puts hospital_info[0]["phone_number"]["phone_number"]
+  #end
 
 end
