aa5f97f772af516a6ae8174ca227216a907bb8bc
Rory Budnick <rorybudnick@gmail.com>
2014-02-18 20:11:29 -0500

    drop down menu working yippee

--- app/controllers/hospitals_controller.rb
+++ app/controllers/hospitals_controller.rb
@@ -1,18 +1,16 @@
 class HospitalsController < ApplicationController
 
   def index
+    @drg_list = HTTParty.get("http://data.cms.gov/resource/97k6-zzx3.json?$select=drg_definition")
+    @drg_list_array = []
+    @drg_list.map { |procedure| @drg_list_array << procedure.values }
+    @drg_list_array.uniq.flatten!
     render(:index)
   end
 
 
   def search
-   @drg_list = HTTParty.get("http://data.cms.gov/resource/97k6-zzx3.json?$select=drg_definition#")
-    @drg_list_array = []
-    @drg_list.map { |procedure| @drg_list_array << procedure.values }
-    @drg_list_array.uniq!
-    @drg_list_array.flatten!
-    @drg_list_array = @drg_list_array.zip(@drg_list_array)
-    binding.pry
+
     @procedure = (params[:drg_definition]).to_s.gsub(" ","+")
     @results = HTTParty.get("http://data.cms.gov/resource/97k6-zzx3.json?$order=average_covered_charges%20ASC&provider_state=#{params[:provider_state]}&drg_definition=#{@procedure}")
     #render(:search)
