2be6b90d213f1eac4a3db1ec0258836cd663e2d1
Rory Budnick <rorybudnick@gmail.com>
2014-06-11 13:36:22 -0400

    fix validation blocking user save

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ActiveRecord::Base
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :dob, presence: true
-  validates :gender, presence: true
+  # validates :dob, presence: true
+  # validates :gender, presence: true
   validates :email, presence: true, uniqueness: true
   self.has_secure_password()
 end
